0667385183dbaee4d0d9291dcc95bc68feac3612
christian miller <cmille47@nd.edu>
2023-10-23 13:21:21 -0400

    user login figured out, could have rrors

---
+++ react-app/src/Common/Services/AuthService.js
@@ -0,0 +1,34 @@
+import Parse from 'parse';
+
+export const createUser = async (user_info) => {
+    const user = new Parse.User();
+
+    user.set("username", user_info.username);
+    user.set("password", user_info.password);
+    user.set("email", user_info.email);
+    user.set("displayName", user_info.displayName);
+
+    return user.signUp().then((newUserSaved) => {
+        return newUserSaved;
+    })
+    .catch((error) => {
+        return error;
+    });
+};
+
+// utilize parse user function for login
+// NOTE: DID NOT CATCH ERROR/RETURN RESPONSE. ERROR WILL BE PASSED ALONG TO AUTHLOGIN.JS
+// WHERE IT CAN MORE APPROPRIATELY BE HANDLED
+export const loginUser = async (user_info) => {
+    return Parse.User.logIn(user_info.email, user_info.password);
+};
+
+export const logoutUser = async () => {
+    return Parse.User.logOut().then(() => {
+        return true;
+    })
+    .catch((error) => {
+        return error;
+    });
+};
+
