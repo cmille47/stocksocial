940baa7ff02be6d4cb6d2e2b71091761eb401a22
Mitchell Trossen <MitchellTrossen@MacBook-Pro-2.attlocal.net>
2023-11-21 11:50:03 -0800

    some styling and more buttons

--- react-app/src/Components/Dashboard/DashboardGood.js
+++ react-app/src/Components/Dashboard/DashboardGood.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useNavigate, useParams } from 'react-router-dom';
-import { getAllUserPortfolios, updatePortfolioCurrentValue, getAllUserPortfoliosWithLeagueNames } from '../../Common/Services/PortfolioService';
+import { Link, useNavigate } from 'react-router-dom';
+// import { useParams } from 'react-router-dom';
+import { updatePortfolioCurrentValue, getAllUserPortfoliosWithLeagueNames } from '../../Common/Services/PortfolioService';
+// import { getAllUserPortfolios, } from '../../Common/Services/PortfolioService';
 import { updatePortfolioPositions } from '../../Common/Services/PositionService';
 import { useAPIFlag } from '../../APIContext';
 import { searchLeaguesByName, getLeagueByName } from '../../Common/Services/LeagueService';
 import '../../Styles/DashboardGood.css';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 import LogoutButton from '../Logout/LogoutButton'; 
 
 
--- react-app/src/Components/League/League.js
+++ react-app/src/Components/League/League.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import { useNavigate } from "react-router-dom"; // useHistory
-import NewLeague from "./NewLeague";
+import { useNavigate } from 'react-router-dom';
+import NewLeague from './NewLeague';
 
 export default function League() {
-
   return (
-    <section>
+    <section className="league-container">
       <h1>Create your League!</h1>
-      <p>This is the League component</p>
       <NewLeague />
-    
-
     </section>
   );
 }
